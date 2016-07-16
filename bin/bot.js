32599e7bce12b0625809e462745afc3cee4d6386
Bryce Hwang <Bryce Hwang>
2016-07-16 11:48:30 -0400

    minor fix

--- bin/bot.js
+++ bin/bot.js
@@ -5,7 +5,7 @@ var botType = "dffdfd"
 if (botType == "norris"){
 	var NorrisBot = require('../lib/norrisbot');
 
-	var token = "xoxb-60007126743-mlEifU3XgNu3SXWwL1RpdSeV", //token for norrisbot
+	var token = "", //token for norrisbot
 		dbPath = process.env.BOT_DB_PATH,
 		name = process.env.BOT_NAME;
 
@@ -17,7 +17,7 @@ if (botType == "norris"){
 	norrisbot.run();
 } else{
 	var TimmyBot = require('../lib/timmybot');
-	var token = "xoxb-59983757393-b7f7wByvAmckMDwm7G27sYn6",//insert API token here
+	var token = "",//insert API token here
 		dbPath = process.env.BOT_DB_PATH,
 		name = process.env.BOT_NAME;
 
